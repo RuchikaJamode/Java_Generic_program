5d43eb5d9b40dfec100ea3f7fbceda5e150a558e
RuchikaJamode <ruchikasjamode@gmail.com>
2024-01-04 23:26:33 +0530

    creating printArray  class and toPrint method

---
+++ Generic_PrintArray.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.generic;
+
+public class Generic_PrintArray {
+
+	
+		// TODO Auto-generated method stub
+public static void toPrint(Integer[] inputArray) {
+	for(int element: inputArray) { System.out.printf("%s", element); }
+
+	System.out.println();
+
+	}
+
+	public static void toPrint (Double[] inputArray) { for(double element: inputArray) { System.out.printf("%s", element); }
+
+	System.out.println();
+
+	}
+
+	public static void toPrint (Character [] inputArray) { for(char element: inputArray) { System.out.printf("%s", element);
+
+	System.out.println();
+
+	}
+
+	
+	}}
---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.generic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] intArray = {1, 2, 3, 4, 5 }; 
+		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.7 };
+
+		Character[] charArray = { 'H', 'E', 'L', 'L', '0' };
+
+		Generic_PrintArray.toPrint(intArray);
+		Generic_PrintArray.toPrint(doubleArray);
+		Generic_PrintArray.toPrint(charArray);
+		
+	}
+
+}
